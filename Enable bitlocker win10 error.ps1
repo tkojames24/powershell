33f8ee15c1951f34c3fb56de089e078d0c8ca48d
tkojames24 <tkojames@ucdavis.edu>
2016-05-11 11:44:08 -0700

    Update Enable bitlocker win10 error.ps1

--- Enable bitlocker win10 error.ps1
+++ Enable bitlocker win10 error.ps1
@@ -1,2 +1,7 @@
+
+//Run this either in CMD or Powershell to enable computer to allow bitlocker to be enabled//
+
+
+
 ﻿powershell.exe -command "New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft -Name FVE; Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\FVE -Name OSEnablePrebootInputProtectorsOnSlates -Value 1 -Type DWord -Force"
 
