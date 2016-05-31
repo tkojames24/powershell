cd9b2342034d367b74737819f60c96d759a470b6
tkojames24 <ejmathison@ucdavis.edu>
2016-05-31 16:54:10 -0700

    Fix blocked by gpo bitlocker start

--- Enable bitlocker win10 error.ps1
+++
@@ -1,6 +0,0 @@
-
-#Run this either in CMD or Powershell to enable computer to allow bitlocker to be enabled #
-
-
-powershell.exe -command "New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft -Name FVE; Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\FVE -Name OSEnablePrebootInputProtectorsOnSlates -Value 1 -Type DWord -Force"
-
