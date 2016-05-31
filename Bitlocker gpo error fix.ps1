19d2c8d2a8de05159ee8a2a2bfa27a4f0473868a
tkojames24 <ejmathison@ucdavis.edu>
2016-05-31 16:55:36 -0700

    Bitlocker will not start gpo error fix

---
+++ Bitlocker gpo error fix.ps1
@@ -0,0 +1,15 @@
+﻿#Run this either in CMD or Powershell to enable computer to allow bitlocker to be enabled off domain #
+
+powershell.exe -command "New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft -Name FVE; Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\FVE -Name OSEnablePrebootInputProtectorsOnSlates -Value 1 -Type DWord -Force"
+
+#Then enable Bitlocker on machine and save recovery key to usb #
+
+# Join back to domain, Now need to back up key to Ad, First find the password in CMD #
+
+manage-bde -protectors -get c:
+
+# Run this in CMD with admin take note of ID #
+
+manage-bde -protectors -adbackup c: -id {}
+
+# check make sure key is in AD # 
