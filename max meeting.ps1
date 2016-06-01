609f9d1859f015066887201552914165ac5817cc
tkojames24 <tkojames@gmail.com>
2016-05-31 21:06:41 -0700

    First Commit

---
+++ max meeting.ps1
@@ -0,0 +1,7 @@
+﻿# default maximum duration in minutes is one day to change first check to see what it is set to currently by running command Get-oCalendarProcessing #
+
+-Identity email | fl 
+
+# to change run following command #
+
+ Set-oCalendarProcessing -Identity email -MaximumDurationInMinutes 50000
